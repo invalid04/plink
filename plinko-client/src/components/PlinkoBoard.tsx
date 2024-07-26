86811cbffe11782b14d169de218fb1279a38b478
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-26 01:35:45 -0500

    dropBall function

--- plinko-client/src/components/PlinkoBoard.tsx
+++ plinko-client/src/components/PlinkoBoard.tsx
@@ -7,6 +7,19 @@ interface Ball {
 }
 
 export default function PlinkoBoard() {
+
+    const [balls, setBalls] = useState<Ball[]>([])
+
+    const dropBall = () => {
+        const newBall: Ball = {
+            id: balls.length,
+            position: 0,
+            left: Math.random() * 90 + 5,
+        }
+        setBalls([...balls, newBall])
+    }
+
+
     return (
         <div className='text-red-400'>
             hi
