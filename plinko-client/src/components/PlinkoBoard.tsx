c9f0de0fb36d8332e68f1b282e834dab9c26e574
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-26 01:33:24 -0500

    board component created and rendered

---
+++ plinko-client/src/components/PlinkoBoard.tsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react'
+
+export default function PlinkoBoard() {
+    return (
+        <div className='text-red-400'>
+            hi
+        </div>
+    )
+}
